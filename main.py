bccbebcba519b82f4499cd72659a5e1ff9af6d75
nagivvar73 <nvvarao@gmail.com>
2023-04-23 15:53:55 +0530

    comt123

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi12090, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi120901, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
